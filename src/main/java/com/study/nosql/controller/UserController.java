5a3023443b1665a5b74a7e3556437508aba5a352
Filipe Kevyn <FilipeKevyn4@gmail.com>
2025-05-01 14:19:42 -0300

    feat: find user by id

--- src/main/java/com/study/nosql/controller/UserController.java
+++ src/main/java/com/study/nosql/controller/UserController.java
@@ -1,14 +1,17 @@
 package com.study.nosql.controller;
 
 import com.study.nosql.domain.User;
+import com.study.nosql.domain.dto.UserDTO;
 import com.study.nosql.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,9 +21,19 @@ public class UserController {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
-        return ResponseEntity.ok().body(service.findAll());
+    public ResponseEntity<List<UserDTO>> findAll(){
+        List<UserDTO> dtoList = service.findAll().stream()
+                .map(user -> new UserDTO(user.getId(), user.getName(), user.getEmail()))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok().body(dtoList);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User user = service.findById(id);
+        var dto = new UserDTO(user.getId(), user.getName(), user.getEmail());
+
+        return ResponseEntity.ok().body(dto);
+    }
 
 }
--- src/main/java/com/study/nosql/services/UserService.java
+++ src/main/java/com/study/nosql/services/UserService.java
@@ -1,6 +1,7 @@
 package com.study.nosql.services;
 
 import com.study.nosql.domain.User;
+import com.study.nosql.exceptions.UserNotFoundException;
 import com.study.nosql.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,9 @@ public class UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    public User findById(String id){
+        return repository.findById(id).orElseThrow(() -> new UserNotFoundException("User com " + id + "não encontrado"));
     }
 }
